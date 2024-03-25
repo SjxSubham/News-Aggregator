bbce4c3f243978037b0eb0d30576656a3285693e
SjxSubham <sjxsubham@gmail.com>
2024-03-25 20:28:21 +0530

    NewsAggregatorModel

---
+++ main/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import News, Category, Comment
+def home(request):
+    first_news=News.objects.first()
+    three_news=News.objects.all()[1:3]
+    three_categories=Category.objects.all()[0:3]
+    return render(request, 'home.html', {
+        'first_news': first_news,
+        'three_news': three_news,
+        'three_categories':three_categories,
+    })
+# All news
+def all_news(request):
+    all_news=News.objects.all()
+    return render(request, 'all-news.html',{
+        'all_news':all_news
+    })
+        
+#detail
+def detail(request,id):
+    news=News.objects.get(pk=id)
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        comment=request.POST['message']
+        Comment.objects.create(
+            news=news,
+            name=name,
+            comment=comment
+        )
+        messages.succes(request,'Comment is succesfully added')
+    category=Category.objects.get(id=news.category.id)
+    rel_news=News.objects.filter(category=category).exclude(id=id)
+    comments=Comment.objects.filter(news=news,status=True).order_by('-id')
+    return render(request, 'detail.html',{
+        'detail':news,
+        'related_news':rel_news,
+        'comments':comments
+    })
+# Fetch
+def all_category(request):
+    cats=Category.objects.all()
+    return render(request, 'category.html',{
+        'cats':cats
+    })
+#fetch all category
+def category(request,id):
+    category=Category.objects.get(id=id)
+    news=News.objects.filter(category=category)
+    return render(request, 'category-news.html',{
+        'all_news':news,
+        'category':category
+    })
