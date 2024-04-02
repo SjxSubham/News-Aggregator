7de5c4219d58dbc8f1040d218526f956a7a57d5c
SjxSubham <sjxsubham@gmail.com>
2024-04-02 21:42:55 +0530

    Use & Customize this as per ur requirements

--- mynews/mynews/settings.py
+++ mynews/mynews/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-us-_u!5mvok&5!yz1hj4kzrjzxi#*9lg1%(=vo)dz#68ojt8_2'
+SECRET_KEY = '/.env/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
