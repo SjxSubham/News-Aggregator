37f791d8bbcca140277172ffaabbe963def4fb0d
SjxSubham <sjxsubham@gmail.com>
2024-04-02 22:24:01 +0530

    update

--- mynews/mynews/settings.py
+++ mynews/mynews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '/.env/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
