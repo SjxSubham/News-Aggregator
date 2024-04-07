7266cda540f1a6d50288cc4c37c038deca1e32db
SjxSubham <sjxsubham@gmail.com>
2024-04-07 18:48:01 +0530

    up

--- mynews/settings.py
+++ mynews/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
