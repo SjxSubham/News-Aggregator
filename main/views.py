ca7048ae78d23c2e7bf95ecf9daf345553653db9
SjxSubham <sjxsubham@gmail.com>
2024-07-08 21:53:52 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import News, Category, Comment
+# example/views.py
+from datetime import datetime
+
+from django.http import HttpResponse
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
 def home(request):
     first_news=News.objects.first()
     three_news=News.objects.all()[1:3]
