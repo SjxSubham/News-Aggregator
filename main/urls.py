ac3f48b9e0c2269d9838871deab4883aafeea30a
SjxSubham <sjxsubham@gmail.com>
2024-07-08 21:55:43 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
+from example.views import index
+
 
 from . import views
 
@@ -11,4 +13,5 @@ urlpatterns = [
     path('detail/<int:id>',views.detail,name='detail'),
     path('all-categories',views.all_category,name='all-category'),
     path('category/<int:id>',views.category,name='category'),
+    path('', index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
